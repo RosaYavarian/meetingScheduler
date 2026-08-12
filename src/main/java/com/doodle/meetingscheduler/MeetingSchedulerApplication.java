6a986833eaa95b8efcd0abd6ed256f1171bab85d
Rosa Yavarian <rosa@MacBookPro.fritz.box>
2026-08-12 12:39:46 +0200

    initialize Spring Boot application

---
+++ src/main/java/com/doodle/meetingscheduler/MeetingSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.doodle.meetingscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(com.doodle.meetingscheduler.MeetingSchedulerApplication.class, args);
+    }
+
+}
